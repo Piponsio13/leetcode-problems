38434f9db04c3d76c2f160f7f89aa2bb50e76ada
Piponsio013 <pipe1910@live.com.mx>
2026-02-12 12:04:08 -0600

    11. Container With Most Water

---
+++ backtracking/two pointers/containerWithMostWater.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size();
+
+        int result = 0;
+        while(l < r){
+            int area = min(height[l], height[r] * (r - l));
+
+            result = max(result, area);
+
+            if(height[l] <= height[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+
+        return result;
+    }
+};
